d3dbb7a928a37209ef77881ad660679e48c8da8f
Esther Edell <esther.edell@gmail.com>
2018-03-01 12:26:16 -0500

    sending it better

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import axiosReq from './axiosReq';
+import Axios from 'axios';
+import AxiosReq from './axiosReq';
+
+
 
 class App extends React.Component {
-    render() {
-      return (
-        <div>
-          TESTING
-        </div>
-      )
-    }
+  render(){
+    return (
+      <AxiosReq />
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
