803e7f717d82d20766acbab988c7647b8f29e101
Jordan Guy <jordan.guy.277@gmail.com>
2018-03-02 16:34:25 -0500

    google map markers

--- dev/scripts/google.js
+++ dev/scripts/google.js
@@ -4,18 +4,25 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 
 export class MapContainer extends React.Component {
-render() {
+    constructor(props){
+        super(props);
+
+    }
+    render() {
     return (
         <Map google={this.props.google} zoom={8}>
 
-            <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
+            {Object.values(this.props.locations).map((location) => {
+                return <Marker name={'Toronto'} title={location.name} position={{ lat: location.latitude, lng: location.longitude  }} onClick={this.onMarkerClick}
+                    name={'Current location'} />
+            })}
 
             <InfoWindow onClose={this.onInfoWindowClose}>
                 <div>
                     {/* <h1>{this.state.selectedPlace.name}</h1> */}
                 </div>
             </InfoWindow>
+        
         </Map>
         );
     }
