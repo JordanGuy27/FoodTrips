42179d5121582f580630cffadcbfb5c32d2d610b
Jordan Guy <jordan.guy.277@gmail.com>
2018-03-02 14:38:02 -0500

    send it

--- dev/scripts/authentication.js
+++ dev/scripts/authentication.js
@@ -145,56 +145,30 @@ class Auth extends React.Component {
                         }
                     }).then((response) => {
                         console.log(response.data.restaurants);
+                        let newArray = Array.from(this.state.restaurants);
+
+                        newArray = response.data.restaurants.map(eatingPlace => {
+                            // console.log(eatingPlace.restaurant.name);
+                            // console.log(eatingPlace.restaurant.location.address);
+
+                            return { 
+                                name: eatingPlace.restaurant.name, 
+                                address: eatingPlace.restaurant.location.address 
+                            };
+                        });
+                        
+                        this.setState({
+                            restaurants: newArray,
+                            lat: data.results[0].geometry.location.lat,
+                            lon: data.results[0].geometry.location.lng
+                        });
                     });
                 
-                return this.setState({
-                    lat: data.results[0].geometry.location.lat,
-                    lon: data.results[0].geometry.location.lng
-                });
                 
                 // console.log(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng);
                 
             });
-        axios
-            .get(`https://developers.zomato.com/api/v2.1/search`, {
-                headers: {
-                    "user-key": `53314a8415a07eafa4656461b1c6272d`
-                },
-                params: {
-                    // q: 'toronto'      
-
-                    lat: this.state.lat,
-                    lon: this.state.lon,
-                    radius: '500',
-
-                    sort: 'real_distance'
-                }
-            })
-            .then(({data}) => {
-                // const restRes = data.restaurants[4].restaurant.name;
-                // // console.log(restRes);
-                // const restAdd = data.restaurants[4].restaurant.location.address;
-
-                // const newList = {
-
-                // };
-
-                const newArray = Array.from(this.state.restaurants);
-
-                data.restaurants.forEach(eatingPlace => {
-                    // console.log(eatingPlace.restaurant.name);
-                    // console.log(eatingPlace.restaurant.location.address);
-
-                    const restObj = { name: eatingPlace.restaurant.name, address: eatingPlace.restaurant.location.address };
-                    newArray.push(restObj);
-                });
-
-                console.log(newArray);
-
-                // this.setState({ 
-                //     restaurants: newArray
-                // });
-            });
+  
     }
     submit(e) {
         e.preventDefault();
