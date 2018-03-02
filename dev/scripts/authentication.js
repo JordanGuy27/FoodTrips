72b68b214481ef851a2fc51b397fa23fcf7046bc
Noel Matta <noelmatta82@gmail.com>
2018-03-02 14:06:38 -0500

    got 2 APIs calling in right order now

--- dev/scripts/authentication.js
+++ dev/scripts/authentication.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import AxiosReq from './axiosReq';
 import axios from 'axios';
 
+const googleURL = "https://maps.googleapis.com/maps/api/geocode/json?";
+
+
 class Auth extends React.Component {
     constructor() {
         super();
@@ -11,7 +14,7 @@ class Auth extends React.Component {
             user: {},
             userText: '',
             lat: '',
-            lng: ''
+            lon: ''
 
         }
         this.signIn = this.signIn.bind(this);
@@ -20,6 +23,8 @@ class Auth extends React.Component {
         this.handleChange = this.handleChange.bind(this);
         this.addRest = this.addRest.bind(this);
         this.submit = this.submit.bind(this);
+        this.getCoords = this.getCoords.bind(this);
+        this.zomatoSearch = this.zomatoSearch.bind(this);
     }
     componentDidMount() {
         firebase.auth().onAuthStateChanged((res) => {
@@ -37,7 +42,47 @@ class Auth extends React.Component {
             }
         });
     }
+  zomatoSearch(lat, lon) {
+    axios
+      .get(`https://developers.zomato.com/api/v2.1/search`, {
+        headers: {
+          "user-key": `53314a8415a07eafa4656461b1c6272d`
+        },
+        params: {
+          // q: 'toronto'      
 
+          'lat': lat,
+          'lon': lon,
+          radius: '50',
+
+          sort: 'real_distance'
+        }
+      })
+      .then(({ data }) => {
+        console.log(data);
+        // const restRes = data.restaurants[4].restaurant.name;
+        // // console.log(restRes);
+        // const restAdd = data.restaurants[4].restaurant.location.address;
+
+        // const newList = {
+
+        // };
+
+        const newArray = Array.from(this.state.restaurants);
+
+        data.restaurants.forEach(eatingPlace => {
+          // console.log(eatingPlace.restaurant.name);
+          // console.log(eatingPlace.restaurant.location.address);
+
+          const restObj = { name: eatingPlace.restaurant.name, address: eatingPlace.restaurant.location.address };
+          newArray.push(restObj);
+        });
+
+        console.log(newArray);
+
+        this.setState({ restaurants: newArray });
+      });
+  }
     signIn(e) {
         const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -72,26 +117,50 @@ class Auth extends React.Component {
             userText: ''
         })
     }
-    submit(e) {
-        e.preventDefault();
-        // const inputResult = `googlecall.com/v1/?query=${inputValue}`
-        const inputResult = this.state.userText;
-        console.log(inputResult);
-        const googleURL = "https://maps.googleapis.com/maps/api/geocode/json?";
+    getCoords(address) {
+        console.log(address);
         axios
             .get(`${googleURL}`, {
                 params: {
                     key: "AIzaSyDNBpAAUuUkRyioDLQUQW_DZYIb1PiY85Q",
-                    address: inputResult
+                    address: address
                 }
             })
             .then(({ data }) => {
-                this.setState({
+                // console.log(data.results[0].geometry.location.lat);
+                console.log(data.results[0].geometry.location.lng);
+                axios
+                    .get(`https://developers.zomato.com/api/v2.1/search`, {
+                        headers: {
+                            "user-key": `53314a8415a07eafa4656461b1c6272d`
+                        },
+                        params: {
+                            // q: 'toronto'      
+
+                            lat: data.results[0].geometry.location.lat,
+                            lon: data.results[0].geometry.location.lng,
+                            radius: '50',
+
+                            sort: 'real_distance'
+                        }
+                    }).then((response) => {
+                        console.log(response.data.restaurants);
+                    });
+                
+                return this.setState({
                     lat: data.results[0].geometry.location.lat,
                     lon: data.results[0].geometry.location.lng
-                });
-                console.log(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng);
+                });                
             });
+    }
+    submit(e) {
+        e.preventDefault();
+        // const inputResult = `googlecall.com/v1/?query=${inputValue}`
+        const inputResult = this.state.userText;
+        const coords = {};
+
+        this.getCoords(inputResult)
+        // this.zomatoSearch(this.state.lat, this.state.lon);
     }
     
     render() {
