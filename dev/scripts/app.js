ac61aaf58a10da431b20930d7432977cdef712c5
Jordan Guy <jordan.guy.277@gmail.com>
2018-03-03 13:27:32 -0500

    google map

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-// import AxiosReq from './axiosReq';
 import Auth from './authentication';
 import MapContainer from './google';
 
